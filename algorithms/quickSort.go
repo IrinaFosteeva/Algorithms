1baaccb1cd3f269c99b4efe04c46b409d4e8939a
IrinaF <ira523523523@gmail.com>
2023-06-19 15:16:31 +0200

    TrueOne

--- algorithms/quickSort.go
+++ algorithms/quickSort.go
@@ -4,20 +4,11 @@ import "fmt"
 
 func Quicksort(arr []int) []int {
 
-	/*dfejfejfhefhjehfjeh
-	sdsdsdsd
-	sdfdggjgj
-	fdssfdg*/
-
 	if len(arr) < 2 {
 		return arr
 	}
 	pivot := arr[0]
 	var less, greater []int
-
-	////SOME
-	//tytyy
-	////CODE
 
 	for _, el := range arr[1:] {
 		if el > pivot {
